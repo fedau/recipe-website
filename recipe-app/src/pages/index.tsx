39321ceea9d457c9e4a7a459bd8dbe16492bca1e
fedau <fedau.d@hotmail.com>
2022-08-17 16:33:33 +0100

    taking away lg:w-2/3 to center grid

--- recipe-app/src/pages/index.tsx
+++ recipe-app/src/pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage<RecipesProps> = ({ recipes }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col min-h-screen p-2 justify-center">
+      <main className="container mx-auto flex flex-col min-h-screen p-2">
         <h1 className="text-3xl md:text-[3rem] leading-normal font-extrabold text-gray-900">
           <span className="text-phatalo">
             Bungalow <div className="text-terra ">Bears</div>
@@ -36,7 +36,7 @@ const Home: NextPage<RecipesProps> = ({ recipes }) => {
           Hiya here are all our recipes! enjoy cooking
         </p>
         <img src="(../assets/main.jpeg)" alt="test image" />
-        <div className=" grid gap-3 pt-3 mt-3 text-center  md:grid-cols-3 lg:grid-cols-5 lg:w-2/3">
+        <div className="grid gap-3 pt-3 mt-3 content-end  md:grid-cols-3 lg:grid-cols-5 ">
           {recipes.map((recipe) => (
             <div className="card" key={recipe.id}>
               <p className="font-bold text-#123524">{recipe?.title}</p>
